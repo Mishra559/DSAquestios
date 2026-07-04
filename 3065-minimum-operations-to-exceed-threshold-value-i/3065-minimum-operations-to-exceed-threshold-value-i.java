99589279d450e367f79e76933b2f5ab3945f80ad
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-04 16:50:23 +0530

    Time: 0 ms (100%), Space: 44.4 MB (88.03%) - LeetHub

---
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+
+        int result = 0;
+
+        for(int x:nums){
+            if(x<k){
+                result++;
+            }
+        }
+
+        return result;
+        
+    }
+}
