4f79b3b1673459aac751c1dc074d185ee7294f54
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-07 13:35:06 +0530

    Time: 1 ms (100%), Space: 43.8 MB (66.47%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String clearDigits(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(Character.isDigit(ch)){
+                if(i>0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+        
+    }
+}
