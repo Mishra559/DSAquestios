0bab167f5de0cf0529a72904cd3e439d50ce56aa
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-10 14:46:24 +0530

    Time: 2 ms (66.63%), Space: 42.8 MB (71.31%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.java
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -3,19 +3,29 @@ class Solution {
 
         int result = 0;
 
+        boolean inside = false;
+
         for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
 
+            // if(ch == '|'){
+            //     i++;
+            //     char ch2 = s.charAt(i);
+            //     while(ch2 != '|'){
+            //         i++;
+            //         ch2 = s.charAt(i);
+            //     }
+            // }
+
+            // else if(ch == '*'){
+            //     result++;
+            // }
+
             if(ch == '|'){
-                i++;
-                char ch2 = s.charAt(i);
-                while(ch2 != '|'){
-                    i++;
-                    ch2 = s.charAt(i);
-                }
+                inside = !inside;
             }
 
-            else if(ch == '*'){
+            else if(ch == '*' && !inside){
                 result++;
             }
 
