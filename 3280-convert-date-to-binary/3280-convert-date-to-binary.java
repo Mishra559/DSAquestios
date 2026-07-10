c215f431a96a504ce5dfd5b99b55d970fb221bd7
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-10 13:29:47 +0530

    Time: 3 ms (94.12%), Space: 44 MB (35.29%) - LeetHub

---
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String convertDateToBinary(String date) {
+
+        String[] arr = date.split("-");
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0;i<arr.length;i++){
+            int y = Integer.parseInt(arr[i]);
+
+            sb.append(Integer.toBinaryString(y));
+            if(i != arr.length-1){
+                sb.append('-');
+            }
+        }
+
+        return sb.toString();
+        
+    }
+}
