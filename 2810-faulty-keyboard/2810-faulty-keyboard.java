320ae34e09ee882399e02f1a706c4a6300811b8d
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-14 16:41:28 +0530

    Time: 3 ms (98.53%), Space: 46.6 MB (31.27%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String finalString(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch:s.toCharArray()){
+            if(ch != 'i'){
+                sb.append(ch);
+            }
+            else{
+                sb.reverse();
+            }
+        }
+
+        return sb.toString();
+        
+    }
+}
