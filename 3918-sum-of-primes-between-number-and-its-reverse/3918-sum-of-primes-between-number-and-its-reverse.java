b93f6224b5b2ed97ffbd3b00acd57f0a3712cbb4
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-14 16:59:06 +0530

    Time: 4 ms (82.42%), Space: 42.8 MB (42.64%) - LeetHub

--- 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.java
+++ 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.java
@@ -1,40 +1,44 @@
 class Solution {
     public int sumOfPrimesInRange(int n) {
 
-
         int temp = n;
         int rev = 0;
 
-        while(temp>0){
-            int d = temp%10;
-            rev = rev*10+d;
-            
-            temp = temp/10;
+        while (temp > 0) {
+            int d = temp % 10;
+            rev = rev * 10 + d;
+
+            temp = temp / 10;
         }
 
-        int a = Math.min(rev,n);
-        int b = Math.max(rev,n);
+        int a = Math.min(rev, n);
+        int b = Math.max(rev, n);
         int sum = 0;
 
-        for(int i = a;i<=b;i++){
+        for (int i = a; i <= b; i++) {
 
-            if(isPrime(i)){
+            if (isPrime(i)) {
                 sum += i;
             }
 
         }
 
         return sum;
-        
+
     }
 
-    private static boolean isPrime(int n){
+    private static boolean isPrime(int n) {
 
         if (n < 2) {
-        return false;
-    }
-        for(int i = 2;i*i<=n;i++){
-            if(n%i == 0){
+            return false;
+        }
+
+        if (n == 2)
+            return true;
+        if (n % 2 == 0)
+            return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
