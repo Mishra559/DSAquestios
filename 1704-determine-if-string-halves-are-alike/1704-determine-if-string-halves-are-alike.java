31919fde108f5291e38f1be9c393acd9520050f1
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-14 15:18:56 +0530

    Time: 2 ms (97.17%), Space: 43 MB (82.05%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -6,12 +6,8 @@ class Solution {
         String s1 = s.substring(0, n / 2);
         String s2 = s.substring(n / 2);
 
-        if(countVowels(s1) == countVowels(s2) && countConsonants(s1) == countConsonants(s2)){
-            return true;
-        }
-
-        return false;
-
+        return countVowels(s1) == countVowels(s2);
+          
     }
 
     private static int countVowels(String str) {
@@ -20,19 +16,6 @@ class Solution {
         for (char ch : str.toCharArray()) {
 
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                count++;
-            }
-
-        }
-        return count;
-    }
-
-    private static int countConsonants(String str) {
-        str = str.toLowerCase();
-        int count = 0;
-        for (char ch : str.toCharArray()) {
-
-            if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
                 count++;
             }
 
