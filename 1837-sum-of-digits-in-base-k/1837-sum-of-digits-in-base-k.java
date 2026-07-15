b61b7bedd78c26a276afaf61f0a2c2cfdc62e76d
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-15 13:29:02 +0530

    Time: 0 ms (100%), Space: 42.2 MB (37.57%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.java
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.java
@@ -1,12 +1,21 @@
 class Solution {
     public int sumBase(int n, int k) {
 
-        String str = Integer.toString(n, k);
+        // String str = Integer.toString(n, k);
+        // int sum = 0;
+
+        // for(char ch:str.toCharArray()){
+        //     int x = ch - '0';
+        //     sum += x;
+        // }
+
+
         int sum = 0;
 
-        for(char ch:str.toCharArray()){
-            int x = ch - '0';
-            sum += x;
+        while(n>0){
+            int d = n % k;
+            sum += d;
+            n = n/k;
         }
 
         return sum;
