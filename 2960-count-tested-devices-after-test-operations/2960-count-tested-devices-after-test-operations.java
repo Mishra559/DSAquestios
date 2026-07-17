fe61c3f8d9a3898b62e98b370161b79378a8d539
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-17 16:16:13 +0530

    Time: 1 ms (59.4%), Space: 45.2 MB (16.29%) - LeetHub

---
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countTestedDevices(int[] nums) {
+
+        int count = 0;
+
+
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] > 0){
+                count++;
+                for(int j = i+1;j<nums.length;j++){
+                    nums[j] = nums[j] - 1;
+                }
+            }
+        }
+        return count;
+        
+    }
+}
