92470a580e210c255c26792cc95e9bb754b2e68a
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-17 15:47:57 +0530

    Time: 0 ms (100%), Space: 42.8 MB (16.8%) - LeetHub

---
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean squareIsWhite(String s) {
+
+        char ch1 = s.charAt(0);
+        char ch2 = s.charAt(1);
+
+        int x = ch1 - 'a' +1;
+        int y = ch2 - '0';
+
+        return (x+y)%2!=0;
+
+
+        
+    }
+}
