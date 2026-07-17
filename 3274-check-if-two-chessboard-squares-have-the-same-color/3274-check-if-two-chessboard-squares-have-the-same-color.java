dbe71283343c3fb36d99ca978796fe3fda1f531d
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-17 19:51:01 +0530

    Time: 0 ms (100%), Space: 43.3 MB (53.43%) - LeetHub

---
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkTwoChessboards(String s1, String s2) {
+        
+        int x1 = s1.charAt(0) - 'a';
+        int y1 = s1.charAt(1) - '0';
+
+        int x2 = s2.charAt(0) - 'a';
+        int y2 = s2.charAt(1) - '0';
+
+
+        if((x1+y1) % 2 == 0 && (x2+y2) % 2 == 0){
+            return true;
+        }
+
+        if((x1+y1) % 2 != 0 && (x2+y2) % 2 != 0){
+            return true;
+        }
+
+        return false;
+
+
+    }
+}
