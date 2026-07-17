1dcd983fc7e79ea95dae09b4096af1b9bf32bfb3
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-17 15:54:17 +0530

    Time: 0 ms (100%), Space: 42.3 MB (97.39%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.java
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.java
@@ -4,10 +4,10 @@ class Solution {
         char ch1 = s.charAt(0);
         char ch2 = s.charAt(1);
 
-        int x = ch1 - 'a' +1;
+        int x = ch1 - 'a';
         int y = ch2 - '0';
 
-        return (x+y)%2!=0;
+        return (x+y)%2==0;
 
 
         
