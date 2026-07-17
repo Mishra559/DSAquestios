33649aadcf178cea2398d4791a476307b5354468
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-17 16:48:21 +0530

    Time: 0 ms (100%), Space: 45.1 MB (16.29%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
@@ -4,12 +4,19 @@ class Solution {
         int count = 0;
 
 
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i] > 0){
+        // for(int i = 0;i<nums.length;i++){
+        //     if(nums[i] > 0){
+        //         count++;
+        //         for(int j = i+1;j<nums.length;j++){
+        //             nums[j] = nums[j] - 1;
+        //         }
+        //     }
+        // }
+
+
+        for(int x:nums){
+            if(x-count >0){
                 count++;
-                for(int j = i+1;j<nums.length;j++){
-                    nums[j] = nums[j] - 1;
-                }
             }
         }
         return count;
