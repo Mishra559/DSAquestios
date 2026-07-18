bf27a5a0d605801f477f02269e897f55abf47150
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-18 14:45:23 +0530

    Time: 2 ms (92.2%), Space: 108.5 MB (88.63%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findArray(int[] pref) {
+
+        if(pref.length == 1){
+            return pref;
+        }
+
+        int[] result = new int[pref.length];
+
+        result[0] = pref[0];
+
+        for(int i = 1;i<pref.length;i++){
+            result[i] = pref[i] ^ pref[i-1];
+        }
+        return result;
+    }
+}
