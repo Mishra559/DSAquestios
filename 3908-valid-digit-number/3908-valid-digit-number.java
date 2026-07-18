3323c20e553bd8be7e77fb5fec10550f6eac2692
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-18 18:14:46 +0530

    Time: 1 ms (98.64%), Space: 42.4 MB (91.87%) - LeetHub

--- 3908-valid-digit-number/3908-valid-digit-number.java
+++ 3908-valid-digit-number/3908-valid-digit-number.java
@@ -1,17 +1,30 @@
 class Solution {
     public boolean validDigit(int n, int x) {
 
-        String str = String.valueOf(n);
-        String y = String.valueOf(x);
+        // String str = String.valueOf(n);
+        // String y = String.valueOf(x);
 
-        if(str.startsWith(y)){
-            return false;
+        // if(str.startsWith(y)){
+        //     return false;
+        // }
+
+        // if(str.contains(y)){
+        //     return true;
+        // }
+        // return false;
+
+        boolean hasX = false;
+        
+
+        while(n>=10){
+            if(n%10 == x){
+                hasX = true;
+            }
+            n = n/10;
         }
 
-        if(str.contains(y)){
-            return true;
-        }
-        return false;
+        
+        return hasX && n != x;
         
     }
 }
