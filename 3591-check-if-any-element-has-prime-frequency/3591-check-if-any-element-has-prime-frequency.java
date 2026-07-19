b420a33431233c0e40274bd24657af998ac1fde5
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-20 01:34:59 +0530

    Time: 1 ms (97.96%), Space: 44 MB (95.62%) - LeetHub

--- 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.java
+++ 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.java
@@ -28,7 +28,7 @@ class Solution {
             return false;
         }
 
-        for(int i = 3;i*i<=x;i++){
+        for(int i = 3;i*i<=x;i+=2){
             if(x%i==0){
                 return false;
             }
