23ee40c4dc478bca3ec4c15591e55d9eadfcc43c
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-20 14:54:11 +0530

    Time: 1 ms (77.81%), Space: 42 MB (89.31%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isThree(int n) {
+
+        if(n<=3){
+            return false;
+        }
+
+        int count  = 2;
+
+        for(int i = 2; i <= n/2 ;i++){
+            if(n%i == 0){
+                count++;
+            }
+        }
+
+       if(count == 3){
+        return true;
+       }
+
+       return false;
+        
+    }
+
+    
+}
