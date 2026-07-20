cde2281f5673255e32f4268d1e96e4245b7fdd14
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-20 15:03:56 +0530

    Time: 0 ms (100%), Space: 41.6 MB (98.59%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -1,24 +1,51 @@
 class Solution {
     public boolean isThree(int n) {
 
-        if(n<=3){
+    //     if(n<=3){
+    //         return false;
+    //     }
+
+    //     int count  = 2;
+
+    //     for(int i = 2; i <= n/2 ;i++){
+    //         if(n%i == 0){
+    //             count++;
+    //         }
+    //     }
+
+    //    if(count == 3){
+    //     return true;
+    //    }
+
+    //    return false;
+
+    int x = (int)Math.sqrt(n);
+
+    if(x*x != n){
+        return false;
+    }
+
+    return isPrime(x);
+     
+    }
+
+    private static boolean isPrime(int x){
+        if(x<2){
+            return false;
+        }
+        if(x == 2){
+            return true;
+        }
+        if(x%2 == 0){
             return false;
         }
 
-        int count  = 2;
-
-        for(int i = 2; i <= n/2 ;i++){
-            if(n%i == 0){
-                count++;
+        for(int i = 3;i*i<=x;i+=2){
+            if(x%i == 0){
+                return false;
             }
         }
-
-       if(count == 3){
         return true;
-       }
-
-       return false;
-        
     }
 
     
