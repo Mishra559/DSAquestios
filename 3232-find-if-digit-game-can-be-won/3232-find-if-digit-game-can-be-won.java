a36b9696d564e8448ad9aeac8eebef6c4cfd87e8
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-21 15:18:16 +0530

    Time: 1 ms (95.21%), Space: 45.6 MB (19.14%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+
+        int singleSum = 0;
+        int doubleSum = 0;
+
+
+        for(int x:nums){
+            if(x>9){
+                doubleSum += x;
+            }
+            else{
+                singleSum += x;
+            }
+        }
+
+        return singleSum>doubleSum || doubleSum > singleSum;
+        
+    }
+}
