be5ce30eab4037b4fed3e0b52abaa7452f199672
Anshul Mishra <94031955+Mishra559@users.noreply.github.com>
2026-07-24 15:08:16 +0530

    Time: 1 ms (94.85%), Space: 46.5 MB (52.3%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.java
+++ 3978-unique-middle-element/3978-unique-middle-element.java
@@ -1,15 +1,31 @@
 class Solution {
     public boolean isMiddleElementUnique(int[] nums) {
 
-        int[] arr = new int[101];
+        // int[] arr = new int[101];
+
+        // for(int x:nums){
+        //     arr[x]++;
+        // }
+
+        // int mid = nums.length/2;
+
+        // return arr[nums[mid]] == 1;
+
+
+        int mid = nums[nums.length/2];
+
+        int count = 0;
 
         for(int x:nums){
-            arr[x]++;
+            if(x == mid){
+                count++;
+                if(count>1){
+                    return false;
+                }
+            }
         }
+        return true;
 
-        int mid = nums.length/2;
-
-        return arr[nums[mid]] == 1;
         
     }
 }
